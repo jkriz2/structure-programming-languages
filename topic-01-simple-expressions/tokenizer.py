252ff9b161b9a24c0a11cb0163c4ad6bad80c9f8
Greg DeLozier <gregdelozier@users.noreply.github.com>
2024-05-28 18:48:13 +0000

    updates to tokenizer to add PMDAS + floats

--- topic-01-simple-expressions/tokenizer.py
+++ topic-01-simple-expressions/tokenizer.py
@@ -8,7 +8,15 @@ break character stream into tokens, provide a list of tokens
 
 import re
 
-patterns = [["\+", "+"], ["\*", "*"], ["\d+", "number"]]
+patterns = [
+    ["\(", "("],
+    ["\)", ")"],
+    ["\*", "*"],
+    ["\/", "/"],
+    ["\+", "+"],
+    ["\-", "-"],
+    ["(\d*\.\d+)|(\d+\.\d*)|(\d+)", "number"],
+]
 
 for pattern in patterns:
     pattern[0] = re.compile(pattern[0])
@@ -27,15 +35,17 @@ def tokenize(characters):
         token = {
             "tag": tag,
             "value": match.group(0),
-            "position": match.end(),
+            "position": position,
         }
-        if token["tag"] == "number":
-            if "." in token["value"]:
-                token["value"] == float(token["value"])
-            else:
-                token["value"] == int(token["value"])
         position = match.end()
         tokens.append(token)
+    for token in tokens:
+        if token["tag"] == "number":
+            if "." in token["value"]:
+                token["value"] = float(token["value"])
+            else:
+                token["value"] = int(token["value"])
+
     return tokens
 
 
@@ -45,13 +55,48 @@ def test_simple_tokens():
     assert tokenize("*")[0]["tag"] == "*"
     tokens = tokenize("*+")
     assert tokens == [
-        {"tag": "*", "value": "*", "position": 1},
+        {"tag": "*", "value": "*", "position": 0},
+        {"tag": "+", "value": "+", "position": 1},
+    ]
+    tokens = tokenize("*/+-()")
+    assert tokens == [
+        {"tag": "*", "value": "*", "position": 0},
+        {"tag": "/", "value": "/", "position": 1},
         {"tag": "+", "value": "+", "position": 2},
+        {"tag": "-", "value": "-", "position": 3},
+        {"tag": "(", "value": "(", "position": 4},
+        {"tag": ")", "value": ")", "position": 5},
     ]
     tokens = tokenize("123")
-    assert tokens == [{"tag": "number", "value": "123", "position": 3}]
+    assert tokens == [{"tag": "number", "value": 123, "position": 0}]
+    tokens = tokenize("123.45")
+    assert tokens == [{"tag": "number", "value": 123.45, "position": 0}]
+    tokens = tokenize("123.")
+    assert tokens == [{"tag": "number", "value": 123.0, "position": 0}]
+    tokens = tokenize(".25")
+    assert tokens == [{"tag": "number", "value": 0.25, "position": 0}]
+
+
+def test_tokenize_expression():
+    tokens = tokenize("(3.5+40)/5-(3.*.4)")
+    assert tokens == [
+        {"tag": "(", "value": "(", "position": 0},
+        {"tag": "number", "value": 3.5, "position": 1},
+        {"tag": "+", "value": "+", "position": 4},
+        {"tag": "number", "value": 40, "position": 5},
+        {"tag": ")", "value": ")", "position": 7},
+        {"tag": "/", "value": "/", "position": 8},
+        {"tag": "number", "value": 5, "position": 9},
+        {"tag": "-", "value": "-", "position": 10},
+        {"tag": "(", "value": "(", "position": 11},
+        {"tag": "number", "value": 3.0, "position": 12},
+        {"tag": "*", "value": "*", "position": 14},
+        {"tag": "number", "value": 0.4, "position": 15},
+        {"tag": ")", "value": ")", "position": 17},
+    ]
 
 
 if __name__ == "__main__":
     test_simple_tokens()
+    test_tokenize_expression()
     print("done.")
