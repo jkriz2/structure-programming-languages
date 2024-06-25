366a703422945362add77211bad99b47a710c2b4
Greg DeLozier <gregdelozier@users.noreply.github.com>
2024-06-25 19:42:06 +0000

    broken mirror code

--- topic-06-functional-programming/functional.py
+++ topic-06-functional-programming/functional.py
@@ -1,6 +1,8 @@
 # ( car '(1 2 3) ) -> 1
 # ( cdr '(1 2 3) ) -> (2 3)
 
+# ( cons (car x) (cdr x) ) --> x
+
 def length(t):
     return len(t)
 
@@ -16,8 +18,8 @@ def tail(t):
     else:
         return []
 
-def append(t, n):
-    return t + [ n ]
+def construct(n, t):
+    return [ n ] + t
 
 def is_list(t):
     return type(t) is list
@@ -25,10 +27,10 @@ def is_list(t):
 # stop defining primitives!
 
 def concat(t, v):
-    if length(v) == 0:
-        return t
+    if length(t) == 0:
+        return v
     else:
-        return concat(append(t, first(v)), tail(v))
+        return construct(first(t), concat(tail(t), v))
 
 def lower(t, n):
     if length(t) == 0:
@@ -75,6 +77,24 @@ def sort2(t):
             concat(sort2(lower(t, first(t))), equal(t,first(t))), sort2(upper(t, first(t)))
         )
 
+def reverse(t):
+    if length(t) == 0:
+        return t
+    else:
+        return concat(reverse(tail(t)), [first(t)])
+
+# broken -- working on it
+def mirror(t):
+    print("t =", t)
+    if not is_list(t):
+        print("t is not a list")
+        return t
+    else:
+        if length(t) == 0:
+            return t
+        else:
+            return concat(mirror(tail(t)), mirror(first(t)))
+
 
 if __name__ == "__main__":
     print(concat([1, 2, 3], [4, 5, 6]))
@@ -83,3 +103,5 @@ if __name__ == "__main__":
     print(sort([1,4,6,7,8,9,3,5,2]))
     print(sort2([1, 4, 6, 7, 8, 9, 3, 4, 5, 2, 7]))
     print(sort2(["cat","apple","banana","dog","zebra","moose"]))
+    print(reverse([1, 4, 6, 7, 8, 9, 3, 4, 5, 2, 7]))
+    print(mirror([1, 4, 6, 7, 8, 9, 3, 4, 5, 2, 7]))
